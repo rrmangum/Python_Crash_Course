a18f7133ac0aa24990af6e81f42d61e027d66c84
Ryan <rrmangum@gmail.com>
2022-08-03 15:28:07 -0700

    Reformatted IAW with PEP 8 Standards

--- Chapter_4.py
+++ Chapter_4.py
@@ -1,23 +1,33 @@
 # Chapter 4 - Working with Lists
 
-# for loops is a useful function for conductin the same action on every item within a list
+# for loops is a useful function for conductin the same action on 
+# every item within a list
 
-magicians = ['alice', 'david', 'carolina'] # define the list
-for magician in magicians: # define a for loop, telling python to pull a name from the list magicians and store it in the variable magician
-    print(magician) # print the name that was just stored in magician
+# define a for loop, telling python to pull a name from the list 
+# magicians and store it in the variable magician
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians: 
+    print(magician)
 
 # python will repeat the for line and the print line fo each name in the list.
 # read this code as 
     # "FOR EVERY MAGICIAN IN THE LIST OF MAGICIANS, PRINT THE MAGICIAN'S NAME"
 
-# looping is very important because it's one of the most common ways a computer automates repetitive tasks
+# looping is very important because it's one of the most common 
+# ways a computer automates repetitive tasks
 
-# for magician in magicians: tells python to retrieve the first value from the list magicians ans store it in the variabale magician. the first value being 'alice'
-# print(magician) tells python to print the current value of magician and then python returns to the first line of the loop
-# python retrieves the next name on the list 'david' and stores that value in magician. python then executes the print function again
+# for magician in magicians: tells python to retrieve the first value from the 
+# list magicians and store it in the variabale magician. the first value being 
+# 'alice'
+# print(magician) tells python to print the current value of magician and then 
+# python returns to the first line of the loop
+# python retrieves the next name on the list 'david' and stores that 
+# value in magician. python then executes the print function again
 
-# the set of steps is repeated for each item in the list, no matter how many items are in the list.
-# keep in mind when writing your own for loops that you can choose any name you want for the temp variable that holds each value in the list.
+# the set of steps is repeated for each item in the list, no matter 
+# how many items are in the list.
+# keep in mind when writing your own for loops that you can choose any name you 
+# want for the temp variable that holds each value in the list.
 # choose something meaningful that represents a single item from the list.
 # for cat in cats:
 # for dog in dogs:
@@ -27,7 +37,8 @@ magicians = ['alice', 'david', 'carolina'] # define the list
 for magician in magicians:
     print(magician.title() + ", that was a great trick!")
 
-# every indented line following the line for magician in magicians is considered inside the loop
+# every indented line following the line for magician in magicians is 
+# considered inside the loop
 # each indented line is executed once for each value in the list.
 
 magicians = ['alice', 'david', 'carolina'] # define the list
@@ -35,10 +46,12 @@ for magician in magicians:
     print(magician.title() + ", that was a great trick!")
     print("I can't wait to see your next trick, " + magician.title() + ".\n")
 
-# usually after completing necessary work with a list you want to summarize a block of output or move on to other work that your program must accomplish
+# usually after completing necessary work with a list you want to summarize a 
+# block of output or move on to other work that your program must accomplish
 print("Thank you, everyone. That was a great magic show!")
 
-# In a logical error the syntax is valid Python code, but the code does not produce the desired result because a problem occurs in its logic
+# In a logical error the syntax is valid Python code, but the code does not 
+# produce the desired result because a problem occurs in its logic
 
 # Exercise 4-1 Pizzas
 '''
@@ -91,15 +104,17 @@ print(numbers)
 # can also use python to skip numbers in a given range
 even_numbers = list(range(2,11,2))
 print(even_numbers)
-# the range function starts with the value 2 and then adds 2 to that value until it reaches or passes the end value, 11.
+# the range function starts with the value 2 and then adds 2 to that value 
+# until it reaches or passes the end value, 11.
 
 squares = [] # empty list
 for value in range(1,11): # loop through each value from 1 to 10
-    square = value**2 # current value is raised to the second power and stored in the variable square
+    square = value**2 # current value is raised to the second power 
     squares.append(square) # new value of square is appended to the list squares
 print(squares)
 
-# clean up the code by omitting the temporary variable square and append each new value directly to the list
+# clean up the code by omitting the temporary variable square and append 
+# each new value directly to the list
 squares = []
 for value in range(1,11):
     squares.append(value**2)
@@ -110,7 +125,9 @@ min(digits)
 max(digits)
 sum(digits)
 
-# list comprehension allows you to generate a list in one line of code by combining a for loop and the creation of new elements ino one line and automatically appends each new element.
+# list comprehension allows you to generate a list in one line of code by 
+# combining a for loop and the creation of new elements ino one line and 
+# automatically appends each new element.
 squares = [value**2 for value in range (1,11)]
 print(squares)
 # define the expression
@@ -128,8 +145,8 @@ print(numbers)
 # Exercise 4-4 One Million
 '''
 Make a list of the numbers from one to one million, and then
-use a for loop to print the numbers. (If the output is taking too long, stop it by
-pressing ctrl-C or by closing the output window.)
+use a for loop to print the numbers. (If the output is taking too long, 
+stop it by pressing ctrl-C or by closing the output window.)
 '''
 numbers = list(range(1,1000001))
 print(numbers)
@@ -166,9 +183,9 @@ for three in threes:
 # Exercise 4-8 Cubes
 '''
 A number raised to the third power is called a cube. For example,
-the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that
-is, the cube of each integer from 1 through 10), and use a for loop to print out
-the value of each cube.
+the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes 
+(that is, the cube of each integer from 1 through 10), and use a for loop to 
+print out the value of each cube.
 '''
 cubes = []
 for value in range(1,11):
@@ -182,14 +199,19 @@ Use a list comprehension to generate a list of the first 10 cubes.
 cubes = [value ** 3 for value in range(1, 11)]
 print(cubes)
 
-# slicing a list by specifying the index of the first and last elements you want to work with
+# slicing a list by specifying the index of the first and 
+# last elements you want to work with
 
+# if you omit the first index in a slice, Python automatically 
+# starts slice at the beinning of the list
+# slice to the end of the list
+# any point on the list to the end even with changes to the list
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[0:3])
 print(players[1:4])
-print(players[:4]) # if you omit the first index in a slice, Python automatically starts slice as the beinning of the list
-print(players[2:]) # slice to the end of the list
-print(players[-3:]) # any point on the list to the end even with changes to the list
+print(players[:4]) 
+print(players[2:]) 
+print(players[-3:]) 
 
 # looping through a slice
 print("Here are the first three players on my team:")
@@ -220,14 +242,14 @@ print(friend_foods)
 Using one of the programs you wrote in this chapter, add several
 lines to the end of the program that do the following:
 
--> Print the message, The first three items in the list are:. Then use a slice to
-print the first three items from that program's list.
+-> Print the message, The first three items in the list are:. Then use a 
+slice to print the first three items from that program's list.
 
 -> Print the message, Three items from the middle of the list are:. Use a slice
 to print three items from the middle of the list.
 
--> Print the message, The last three items in the list are:. Use a slice to print
-the last three items in the list
+-> Print the message, The last three items in the list are:. Use a slice to 
+print the last three items in the list
 '''
 print("The first three items in the list are:")
 print(players[0:3])
@@ -249,9 +271,10 @@ Then, do the following:
 -> Add a different pizza to the list friend_pizzas.
 
 -> Prove that you have two separate lists. Print the message, My favorite
-pizzas are:, and then use a for loop to print the first list. Print the message,
-My friend's favorite pizzas are:, and then use a for loop to print the second list. 
-Make sure each new pizza is stored in the appropriate list.
+pizzas are:, and then use a for loop to print the first list. 
+Print the message, My friend's favorite pizzas are:, and then use a 
+for loop to print the second list. Make sure each new pizza is 
+stored in the appropriate list.
 '''
 favorite_pizza = ["supreme", "pepperoni", "margherita"]
 friend_favorite_pizza = favorite_pizza[:]
@@ -274,7 +297,8 @@ dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
 
-# you can't change items withing a tuple, but you can assign a new value to a variable that holds a tuple
+# you can't change items withing a tuple, but you can assign a new value 
+# to a variable that holds a tuple
 
 print("Original dimensions:")
 for dimension in dimensions:
