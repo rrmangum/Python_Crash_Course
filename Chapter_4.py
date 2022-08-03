f6cd87181f37158ee5219e0e81099a9a9ab2d5f3
Ryan <rrmangum@gmail.com>
2022-08-03 15:52:49 -0700

    Reformatted IAW PEP 8 Standards

--- Chapter_4.py
+++ Chapter_4.py
@@ -242,10 +242,10 @@ print(friend_foods)
 Using one of the programs you wrote in this chapter, add several
 lines to the end of the program that do the following:
 
--> Print the message, The first three items in the list are:. Then use a 
+-> Print the message, The first three items in the list are: Then use a 
 slice to print the first three items from that program's list.
 
--> Print the message, Three items from the middle of the list are:. Use a slice
+-> Print the message, Three items from the middle of the list are: Use a slice
 to print three items from the middle of the list.
 
 -> Print the message, The last three items in the list are:. Use a slice to 
